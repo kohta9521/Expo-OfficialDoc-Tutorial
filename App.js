8856f62561196ab04422127620e912a12104f029
k_95.21 <kohta0421kochi@gmail.com>
2024-01-05 12:29:58 +0900

    add iamge picker library

--- App.js
+++ App.js
@@ -3,14 +3,34 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 
 // components import
 import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+// image picker import
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View styles={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -31,5 +51,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
